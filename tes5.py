2ef3b47b854d5053b65191a4c8c81074bf1f5ee8
PanjiSri <neiabaraja04@gmail.com>
2024-02-12 21:14:58 +0700

    Testing Fungsi Find Move

--- tes5.py
+++ tes5.py
@@ -1,13 +1,30 @@
 def find_move(arah, titik, matriks, validasi):
     baris = len(matriks)
-    kolom = len(matriks)
+    kolom = len(matriks[0])
     move = []
     if arah == 'V':
-        for i in range(kolom):
+        for i in range(baris):
             if validasi[i][titik[1]] == False and (i, titik[1]) != titik:
                 move.append((i,titik[1]))
     elif arah == 'H':
-        for j in range(baris):
+        for j in range(kolom):
+            #print("ini j", j)
             if validasi[titik[0]][j] == False and (titik[0], j) != titik:
                 move.append((titik[0],j))
     return move
+
+
+#Ngecek fungsi titik
+matriks = [['A','B'],
+           ['C','D'],
+           ['E','F']
+           ]
+validasi = [[False, False],
+            [False, False],
+            [False, False]
+            ]
+titik = (1,1)
+arah = 'H'
+move = find_move(arah, titik, matriks, validasi)
+
+print(move)
